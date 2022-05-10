4d0b80b9475250c8d7167b03cfc326681f79b8da
pyou20 <cse.pyou20@gzu.edu.cn>
2022-05-10 13:57:08 +0800

    变更

--- src/Study_interface/Test2.java
+++ src/Study_interface/Test2.java
@@ -2,7 +2,37 @@ package Study_interface;
 
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("测试");
-        System.out.println("再来一次");
+        BB b=new BB();
+        b.showA();
+        b.showB10();
+        b.showC10();
+    }
+}
+
+interface AA{
+    void showA();
+    private void show10(String str){
+        int i=0;
+        while (i<10){
+            System.out.print(str+" ");
+            i++;
+        }
+        System.out.println(" ");
+    }
+
+    default void showB10(){
+        show10("BBBB");
+    }
+
+    default void showC10(){
+        show10("CCCC");
+    }
+}
+
+class BB implements AA{
+
+    @Override
+    public void showA() {
+        System.out.println("AAAA");
     }
 }
